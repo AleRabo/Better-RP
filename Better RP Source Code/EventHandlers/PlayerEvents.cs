d22c1d0b1588674d312e01dbca0c7695232e9e65
AleRabo <83182793+AleRabo@users.noreply.github.com>
2021-06-21 20:03:13 +0200

    Revert "Read the new code and try to know what I wrote"

---
+++ Better RP Source Code/EventHandlers/PlayerEvents.cs
@@ -0,0 +1,167 @@
+namespace BetterRP.EventHandlers
+{
+    using System;
+    using System.Collections.Generic;
+    using Exiled.API.Features;
+    using Exiled.Events.EventArgs;
+    using Respawning;
+    using Hints;
+    using CustomPlayerEffects;
+    using MEC;
+    using UnityEngine;
+    using Exiled.API.Extensions;
+    using Exiled.Bootstrap;
+    using Exiled.Events.Extensions;
+    using UnityEditor;
+    using UnityEngineInternal;
+    using UnityStandardAssets;
+    
+
+    public class PlayerEvents
+    {
+        public static void OnBrokingElevator(InteractingElevatorEventArgs ev)
+        {
+            if (new System.Random().Next(0, 101) <= Plugin.Instance.Config.ElevatorBrokenChance)
+            {
+                if (Plugin.Instance.Config.ElevatorBrokeAfflictScps)
+                {
+                    if (ev.Player.Team == Team.SCP)
+                    {
+                        ev.Player.Hurt(400, DamageTypes.Falldown);
+                        ev.Player.Broadcast(6, Plugin.Instance.Config.broken_elevator);
+                    }
+
+                    if (ev.Player.IsHuman)
+                    {
+                        ev.Player.Kill(DamageTypes.Falldown);
+                        ev.Player.Broadcast(6, Plugin.Instance.Config.broken_elevator);
+                    }
+                }
+            }
+        }
+
+
+    public static void OnInteractingBlockedDoor(InteractingDoorEventArgs ev)
+        {
+            if (ev.IsAllowed == false)
+            {
+                ev.Player.ShowHint(Plugin.Instance.Config.InteractingBlockedDoor, 6);
+            }
+        }
+
+        public static void OnTriggeringTesla(TriggeringTeslaEventArgs ev)
+        {
+            if (Plugin.Instance.Config.TeslagateBypassWithTablet)
+            {
+                if (ev.Player.HasItem(ItemType.WeaponManagerTablet))
+                {
+
+                    ev.IsTriggerable = false;
+
+                }
+                if (ev.Player.CurrentItem.id == ItemType.WeaponManagerTablet)
+                {
+                    ev.Player.ShowHint(Plugin.Instance.Config.TeslaGatebypasstHint, 3);
+                }
+            }
+        }
+
+        public static void OnActivatingWarheadPanel(ActivatingWarheadPanelEventArgs ev)
+        {
+            ev.Player.ShowHint(Plugin.Instance.Config.ActivatingWarheadPanel, 6);
+        }
+
+        public static void OnPlayerHeal(UsedMedicalItemEventArgs ev)
+        {
+            if (ev.Item == ItemType.SCP268)
+            {
+                return;
+            }
+            ev.Player.ShowHint(Plugin.Instance.Config.PlayerHealHint, 6);
+        }
+
+
+        public void OnHurting(HurtingEventArgs ev)
+        {
+
+            if (Plugin.Instance.Config.DamageIndicatorIsEnabled == false)
+            {
+                return;
+            }
+
+            if (ev.IsAllowed == false)
+            {
+                return;
+            }
+
+
+            if (ev.Target == null)
+            {
+                return;
+            }
+
+            if (ev.Target.IsDead || ev.Target.IsInvisible || ev.Amount < 0)
+            {
+                return;
+            }
+
+            if (ev.Target == ev.Attacker)
+            {
+                return;
+            }
+
+            ev.Attacker.ShowHint(Plugin.Instance.Config.DamageIndicatorHint.Replace("%damage", ev.Amount.ToString()).Replace("%player", ev.Target.Nickname), 6);
+        }
+        public static void OnCuffingSCP(HandcuffingEventArgs ev)
+        {
+            if (Plugin.Instance.Config.HumanCanCuffSCPS == true)
+            {
+                if (ev.Cuffer.IsHuman && ev.Target.Team == Team.SCP && true)
+                {
+                    if (ev.Target.Role == RoleType.Scp93989)
+                    {
+                        ev.IsAllowed = Plugin.Instance.Config.Scps939;
+                    }
+
+                    if (ev.Target.Role == RoleType.Scp93953)
+                    {
+                        ev.IsAllowed = Plugin.Instance.Config.Scps939;
+                    }
+
+                    if (ev.Target.Role == RoleType.Scp173)
+                    {
+                        ev.IsAllowed = Plugin.Instance.Config.Scp173;
+                    }
+
+                    if (ev.Target.Role == RoleType.Scp049)
+                    {
+                        ev.IsAllowed = Plugin.Instance.Config.Scp049;
+                    }
+
+                    if (ev.Target.Role == RoleType.Scp0492)
+                    {
+                        ev.IsAllowed = Plugin.Instance.Config.Scp0492;
+                    }
+
+                    if (ev.Target.Role == RoleType.Scp096)
+                    {
+                        ev.IsAllowed = Plugin.Instance.Config.Scp096;
+                    }
+
+                    ev.IsAllowed = true;
+
+
+                }
+
+            }
+
+
+
+        }
+
+          
+    }
+}
+
+
+
--- Better RP Source Code/Plugin.cs
+++ Better RP Source Code/Plugin.cs
@@ -1,45 +1,76 @@
-using Exiled.API.Features;
-using PlayerHandlers = Exiled.Events.Handlers.Player;
-using ServerHandlers = Exiled.Events.Handlers.Server;
-
 namespace BetterRP
 {
+    using BetterRP.EventHandlers;
+    using System;
+    using Exiled.API.Enums;
+    using Exiled.API.Features;
+    using PlayerHandlers = Exiled.Events.Handlers.Player;
+    using ServerHandlers = Exiled.Events.Handlers.Server;
+
+    /// <summary>
+    /// The main plugin class.
+    /// </summary>
     public class Plugin : Plugin<Config>
     {
-        public static Plugin Singleton;
-        
-        public override string Prefix => "BetterRP";
+        private static readonly Plugin InstanceValue = new Plugin();
 
-        private EventHandlers EventHandlers;
+        private Plugin()
+        {
+        }
 
+        /// <summary>
+        /// Gets an instance of the <see cref="Plugin"/> class.
+        /// </summary>
+        public static Plugin Instance { get; } = InstanceValue;
+
+        /// <inheritdoc />
+        public override string Prefix { get; } = "BetterRP";
+
+        /// <summary>
+        /// Gets an instance of the <see cref="EventHandlers.PlayerEvents"/> class.
+        /// </summary>
+        public PlayerEvents PlayerEvents { get; private set; }
+
+        /// <summary>
+        /// Gets an instance of the <see cref="EventHandlers.ServerEvents"/> class.
+        /// </summary>
+        public ServerEvents ServerEvents { get; private set; }
+
+        /// <inheritdoc />
         public override void OnEnabled()
         {
-            Singleton = this;
-            EventHandlers = new EventHandlers();
-            ServerHandlers.RoundStarted += EventHandlers.ScpBreakContainmentAnnouncement;
-            PlayerHandlers.TriggeringTesla += EventHandlers.OnTriggeringTesla;
-            PlayerHandlers.Hurting += EventHandlers.OnHurting;
-            PlayerHandlers.InteractingElevator += EventHandlers.OnBrokingElevator;
-            PlayerHandlers.InteractingDoor += EventHandlers.OnInteractingBlockedDoor;
-            PlayerHandlers.ActivatingWarheadPanel += EventHandlers.OnActivatingWarheadPanel;
-            PlayerHandlers.MedicalItemUsed += EventHandlers.OnPlayerHeal;
-            PlayerHandlers.Handcuffing += EventHandlers.OnCuffingSCP;
+            ServerEvents = new ServerEvents();
+            PlayerEvents = new PlayerEvents();
+
+            ServerHandlers.RoundStarted += ServerEvents.OnRoundStarted;
+
+            PlayerHandlers.TriggeringTesla += PlayerEvents.OnTriggeringTesla;
+            PlayerHandlers.Hurting += PlayerEvents.OnHurting;
+            PlayerHandlers.InteractingElevator += PlayerEvents.OnBrokingElevator;
+            PlayerHandlers.InteractingDoor += PlayerEvents.OnInteractingBlockedDoor;
+            PlayerHandlers.ActivatingWarheadPanel += PlayerEvents.OnActivatingWarheadPanel;
+            PlayerHandlers.MedicalItemUsed += PlayerEvents.OnPlayerHeal;
+            PlayerHandlers.Handcuffing += PlayerEvents.OnCuffingSCP;
+
             base.OnEnabled();
         }
-        
+
+        /// <inheritdoc />
         public override void OnDisabled()
         {
-            ServerHandlers.RoundStarted -= EventHandlers.ScpBreakContainmentAnnouncement;
-            PlayerHandlers.TriggeringTesla -= EventHandlers.OnTriggeringTesla;
-            PlayerHandlers.Hurting -= EventHandlers.OnHurting;
-            PlayerHandlers.InteractingElevator -= EventHandlers.OnBrokingElevator;
-            PlayerHandlers.InteractingDoor -= EventHandlers.OnInteractingBlockedDoor;
-            PlayerHandlers.ActivatingWarheadPanel -= EventHandlers.OnActivatingWarheadPanel;
-            PlayerHandlers.MedicalItemUsed -= EventHandlers.OnPlayerHeal;
-            PlayerHandlers.Handcuffing -= EventHandlers.OnCuffingSCP;
-            EventHandlers = null;
-            Singleton = null;
-            base.OnDisabled();
+
+            ServerHandlers.RoundStarted -= ServerEvents.OnRoundStarted;
+
+            PlayerHandlers.TriggeringTesla -= PlayerEvents.OnTriggeringTesla;
+            PlayerHandlers.Hurting -= PlayerEvents.OnHurting;
+            PlayerHandlers.InteractingElevator -= PlayerEvents.OnBrokingElevator;
+            PlayerHandlers.InteractingDoor -= PlayerEvents.OnInteractingBlockedDoor;
+            PlayerHandlers.ActivatingWarheadPanel -= PlayerEvents.OnActivatingWarheadPanel;
+            PlayerHandlers.MedicalItemUsed -= PlayerEvents.OnPlayerHeal;
+            PlayerHandlers.Handcuffing -= PlayerEvents.OnCuffingSCP;
+
+            PlayerEvents = null;
+            ServerEvents = null;
         }
     }
 }
