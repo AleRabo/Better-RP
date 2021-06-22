0e324dc5621a8f0d6ea5260231de0fd370bb9e4f
AleRabo <83182793+AleRabo@users.noreply.github.com>
2021-06-22 13:33:51 +0200

    Update Plugin.cs

--- Better RP Source Code/Plugin.cs
+++ Better RP Source Code/Plugin.cs
@@ -1,76 +1,53 @@
+
+using Exiled.API.Features;
+using PlayerHandlers = Exiled.Events.Handlers.Player;
+using ServerHandlers = Exiled.Events.Handlers.Server;
+
 namespace BetterRP
 {
-    using BetterRP.EventHandlers;
-    using System;
-    using Exiled.API.Enums;
-    using Exiled.API.Features;
-    using PlayerHandlers = Exiled.Events.Handlers.Player;
-    using ServerHandlers = Exiled.Events.Handlers.Server;
-
-    /// <summary>
-    /// The main plugin class.
-    /// </summary>
     public class Plugin : Plugin<Config>
     {
-        private static readonly Plugin InstanceValue = new Plugin();
 
-        private Plugin()
-        {
-        }
+        // The singleton of the plugin
+        public static Plugin Singleton;
 
-        /// <summary>
-        /// Gets an instance of the <see cref="Plugin"/> class.
-        /// </summary>
-        public static Plugin Instance { get; } = InstanceValue;
 
-        /// <inheritdoc />
-        public override string Prefix { get; } = "BetterRP";
+        public override string Prefix => "BetterRP";
+
 
         /// <summary>
-        /// Gets an instance of the <see cref="EventHandlers.PlayerEvents"/> class.
+        /// The event handlers <see cref="BetterRP.EventHandlers"/> class.
         /// </summary>
-        public PlayerEvents PlayerEvents { get; private set; }
+        private EventHandlers EventHandlers;
 
-        /// <summary>
-        /// Gets an instance of the <see cref="EventHandlers.ServerEvents"/> class.
-        /// </summary>
-        public ServerEvents ServerEvents { get; private set; }
-
-        /// <inheritdoc />
         public override void OnEnabled()
         {
-            ServerEvents = new ServerEvents();
-            PlayerEvents = new PlayerEvents();
-
-            ServerHandlers.RoundStarted += ServerEvents.OnRoundStarted;
-
-            PlayerHandlers.TriggeringTesla += PlayerEvents.OnTriggeringTesla;
-            PlayerHandlers.Hurting += PlayerEvents.OnHurting;
-            PlayerHandlers.InteractingElevator += PlayerEvents.OnBrokingElevator;
-            PlayerHandlers.InteractingDoor += PlayerEvents.OnInteractingBlockedDoor;
-            PlayerHandlers.ActivatingWarheadPanel += PlayerEvents.OnActivatingWarheadPanel;
-            PlayerHandlers.MedicalItemUsed += PlayerEvents.OnPlayerHeal;
-            PlayerHandlers.Handcuffing += PlayerEvents.OnCuffingSCP;
-
+            Singleton = this;
+            EventHandlers = new EventHandlers();
+            ServerHandlers.RoundStarted += EventHandlers.ScpBreakContainmentAnnouncement;
+            PlayerHandlers.TriggeringTesla += EventHandlers.OnTriggeringTesla;
+            PlayerHandlers.Hurting += EventHandlers.OnHurting;
+            PlayerHandlers.InteractingElevator += EventHandlers.OnBrokingElevator;
+            PlayerHandlers.InteractingDoor += EventHandlers.OnInteractingBlockedDoor;
+            PlayerHandlers.ActivatingWarheadPanel += EventHandlers.OnActivatingWarheadPanel;
+            PlayerHandlers.MedicalItemUsed += EventHandlers.OnPlayerHeal;
+            PlayerHandlers.Handcuffing += EventHandlers.OnCuffingSCP;
             base.OnEnabled();
         }
 
-        /// <inheritdoc />
         public override void OnDisabled()
         {
-
-            ServerHandlers.RoundStarted -= ServerEvents.OnRoundStarted;
-
-            PlayerHandlers.TriggeringTesla -= PlayerEvents.OnTriggeringTesla;
-            PlayerHandlers.Hurting -= PlayerEvents.OnHurting;
-            PlayerHandlers.InteractingElevator -= PlayerEvents.OnBrokingElevator;
-            PlayerHandlers.InteractingDoor -= PlayerEvents.OnInteractingBlockedDoor;
-            PlayerHandlers.ActivatingWarheadPanel -= PlayerEvents.OnActivatingWarheadPanel;
-            PlayerHandlers.MedicalItemUsed -= PlayerEvents.OnPlayerHeal;
-            PlayerHandlers.Handcuffing -= PlayerEvents.OnCuffingSCP;
-
-            PlayerEvents = null;
-            ServerEvents = null;
+            ServerHandlers.RoundStarted -= EventHandlers.ScpBreakContainmentAnnouncement;
+            PlayerHandlers.TriggeringTesla -= EventHandlers.OnTriggeringTesla;
+            PlayerHandlers.Hurting -= EventHandlers.OnHurting;
+            PlayerHandlers.InteractingElevator -= EventHandlers.OnBrokingElevator;
+            PlayerHandlers.InteractingDoor -= EventHandlers.OnInteractingBlockedDoor;
+            PlayerHandlers.ActivatingWarheadPanel -= EventHandlers.OnActivatingWarheadPanel;
+            PlayerHandlers.MedicalItemUsed -= EventHandlers.OnPlayerHeal;
+            PlayerHandlers.Handcuffing -= EventHandlers.OnCuffingSCP;
+            EventHandlers = null;
+            Singleton = null;
+            base.OnDisabled();
         }
     }
 }
