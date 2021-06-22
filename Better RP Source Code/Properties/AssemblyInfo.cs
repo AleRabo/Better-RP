85a1e4a7c6dea1e92602ec8097a8b52a2c57976e
AleRabo <83182793+AleRabo@users.noreply.github.com>
2021-06-22 13:35:31 +0200

    Update AssemblyInfo.cs

--- Better RP Source Code/Properties/AssemblyInfo.cs
+++ Better RP Source Code/Properties/AssemblyInfo.cs
@@ -1,36 +1,37 @@
-﻿using System.Reflection;
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Le informazioni generali relative a un assembly sono controllate dal seguente 
-// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
-// associate a un assembly.
+// General information about an assembly is controlled by the following
+// set of attributes. Change the values ​​of these attributes to change the information
+// associated with an assembly.
 [assembly: AssemblyTitle("Better RP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This plugin make better the RolePlay (i think)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AleRabo")]
 [assembly: AssemblyProduct("Better RP")]
 [assembly: AssemblyCopyright("Copyright © Ale Rabo  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
-// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
-// COM, impostare su true l'attributo ComVisible per tale tipo.
+//  If you set ComVisible to false, the types in this assembly will not be visible
+//  to the COM components. If you need to access a type in this assembly from
+//  COM, set the ComVisible attribute for that type to true.
 [assembly: ComVisible(false)]
 
-// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+// If the project is exposed to COM, the following GUID will be used as the type library of the type
 [assembly: Guid("bbba9162-bf89-4a73-a663-539ebcb8673e")]
 
-// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+// The version information of an assembly consists of the following four values:
 //
-//      Versione principale
-//      Versione secondaria
-//      Numero di build
-//      Revisione
+//      Major version
+//      Minor version
+//      Build number
+//      Revision
 //
-// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
-// usando l'asterisco '*' come illustrato di seguito:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+// You can specify all values ​​or set default values ​​for revision and build numbers
+// using the asterisk '*' as shown below:
+// [assembly: AssemblyVersion ("1.0. *")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
