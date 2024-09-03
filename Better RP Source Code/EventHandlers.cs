d9419fbb3d3b83e33de6db1837a06f21f3b805fc
AleRabo <83182793+AleRabo@users.noreply.github.com>
2024-09-03 17:27:14 +0200

    Update EventHandlers.cs

--- Better RP Source Code/EventHandlers.cs
+++ Better RP Source Code/EventHandlers.cs
@@ -2,100 +2,104 @@ using System.Collections.Generic;
 using System.Linq;
 using Exiled.API.Enums;
 using Exiled.API.Features;
-using Exiled.Events.EventArgs;
+using Exiled.API.Features.Roles;
+using Exiled.Events.EventArgs.Player;
 using MEC;
-using UnityEngine;
+using PlayerRoles;
 
 namespace BetterRP
 {
     public class EventHandlers
     {
         Role scpsubject;
+        bool teslaEnabled = true;
+        Exiled.API.Features.TeslaGate tesla;
 
         // The scp annuncement when the round start (not configurable via config)
         public void ScpBreakContainmentAnnouncement()
         {
-            var SCPs = Player.List.Where(x => x.Side == Side.Scp);
-            var pronounciations = new Dictionary<RoleType, string>
+
+        var SCPs = Player.List.Where(x => x.LeadingTeam == LeadingTeam.Anomalies);
+            var pronounciations = new Dictionary<RoleTypeId, string>
             {
-                { RoleType.Scp049, "SCP 0 4 9"},
-                { RoleType.Scp079, "SCP 0 7 9" },
-                { RoleType.Scp096, "SCP 0 9 6" },
-                { RoleType.Scp106, "SCP 1 0 6" },
-                { RoleType.Scp173, "SCP 1 7 3" },
-                { RoleType.Scp93953, "SCP 9 3 9 5 3" },
-                { RoleType.Scp93989, "SCP 9 3 9 8 9" }
+                { RoleTypeId.Scp049, "SCP 0 4 9"},
+                { RoleTypeId.Scp079, "SCP 0 7 9" },
+                { RoleTypeId.Scp096, "SCP 0 9 6" },
+                { RoleTypeId.Scp106, "SCP 1 0 6" },
+                { RoleTypeId.Scp173, "SCP 1 7 3" },
+                { RoleTypeId.Scp939, "SCP 9 3 9" },
             };
             var text = SCPs.Aggregate("", (current, scp) => current + $" {pronounciations[scp.Role]}");
             if (string.IsNullOrEmpty(text))
                 Cassie.DelayedMessage($"attention all personnel detected {SCPs.Count()} scpsubject breach {text}", 5);
         }
 
-        // The elevator broke
-        public void OnBrokingElevator(InteractingElevatorEventArgs ev)
-        {
-            if (ev.Lift.InRange(ev.Player.Position, out GameObject gameObject) && Random.Range(0, 101) <= Plugin.Singleton.Config.ElevatorBrokenChance)
-            {
-                    if (Plugin.Singleton.Config.ElevatorBrokeAfflictScps && ev.Player.IsScp)
-                    {
-                        Timing.CallDelayed(2.0f, () => ev.Player.Hurt(400, DamageTypes.Falldown));
-                        Timing.CallDelayed(2.0f, () => ev.Player.Broadcast(6, Plugin.Singleton.Config.broken_elevator));
-                    }
-
-                    if (ev.Player.IsHuman)
-                    {
-                        Timing.CallDelayed(2.0f, () => ev.Player.Kill(DamageTypes.Falldown));
-                        Timing.CallDelayed(2.0f, () => ev.Player.Broadcast(6, Plugin.Singleton.Config.broken_elevator));
-                    }
-            }
-        }
-
         // The hint that show up when someone interact with a blocked door
         public void OnInteractingBlockedDoor(InteractingDoorEventArgs ev)
         {
             if (!ev.IsAllowed)
-                ev.Player.ShowHint(Plugin.Singleton.Config.InteractingBlockedDoor, 6);
+                ev.Player.ShowHint(BetterRP.Singleton.Config.InteractingBlockedDoor, 6);
         }
 
         // The bypass for tesla gate with a tablet in his inventory
         public void OnTriggeringTesla(TriggeringTeslaEventArgs ev)
         {
-            if (Plugin.Singleton.Config.TeslagateBypassWithTablet && ev.Player.HasItem(ItemType.WeaponManagerTablet))
+            tesla = ev.Tesla;
+            if (BetterRP.Singleton.Config.AutoTeslaBypasss)
             {
-                // The hint that show up when a player bypass a tesla gate
-                ev.Player.ShowHint(Plugin.Singleton.Config.TeslaGatebypasstHint);
+                if (ev.Player.Inventory.UserInventory.Items.Any(item => BetterRP.Singleton.Config.TeslagateBypassItem.Contains(item.Value.ItemTypeId)))
+                { 
+                    ev.DisableTesla = true;
+                }
+            }
+            else
+            { 
+                if (teslaEnabled)
+                {
+                    ev.DisableTesla = false;
+                }
+                else
+                {
+                  // The hint that show up when a player bypass a tesla gate
+                    ev.Player.ShowHint(BetterRP.Singleton.Config.TeslaGatebypasstHint);
 
-                
+                    ev.DisableTesla = true;
+                }
+            }
+        }
 
-                ev.IsTriggerable = false;
+        public void OnThrowing(DroppingItemEventArgs ev)
+        {
+            if (!BetterRP.Singleton.Config.AutoTeslaBypasss && ev.IsThrown && BetterRP.Singleton.Config.TeslagateBypassItem.Contains(ev.Item.Type) && tesla.IsPlayerInIdleRange(ev.Player))
+            {
+                ev.IsAllowed = false;
+                if (teslaEnabled)
+                {
+                    teslaEnabled = false;
+                }
+                else
+                {
+                    teslaEnabled = true;
+                }
             }
         }
 
         // The hint that show up when someone activate the WarHead panel
         public void OnActivatingWarheadPanel(ActivatingWarheadPanelEventArgs ev)
-            => ev.Player.ShowHint(Plugin.Singleton.Config.ActivatingWarheadPanel, 6);
+            => ev.Player.ShowHint(BetterRP.Singleton.Config.ActivatingWarheadPanel, 6);
 
         // The hint that show up when someone heal himself
         public void OnPlayerHeal(UsedItemEventArgs ev)
         {
-            if (ev.Item.Type == ItemType.Medkit && ev.Item.Type == ItemType.Painkillers)
-                Timing.CallDelayed(2.0f, () =>  ev.Player.ShowHint(Plugin.Singleton.Config.PlayerHealHint, 6));
+            if (ev.Item.Type == ItemType.Medkit || ev.Item.Type == ItemType.Painkillers)
+                Timing.CallDelayed(2.0f, () => ev.Player.ShowHint(BetterRP.Singleton.Config.PlayerHealHint, 6));
         }
 
         // The damage indicator
         public void OnHurting(HurtingEventArgs ev)
         {
-            if (Plugin.Singleton.Config.DamageIndicatorIsEnabled && ev.IsAllowed && ev.Target != null && !ev.Target.IsDead && !ev.Target.IsInvisible && ev.Amount > 0 && ev.Target != ev.Attacker)
-                ev.Attacker.ShowHint(Plugin.Singleton.Config.DamageIndicatorHint.Replace("%damage", ev.Amount.ToString()).Replace("%player", ev.Target.Nickname), 6);
-        }
-
-        // The ability for human to cuff scps
-        public void OnCuffingSCP(HandcuffingEventArgs ev)
-        {
-            if (ev.Target.Team == Team.SCP)
-            {
-                ev.IsAllowed = Plugin.Singleton.Config.SCPRoles.Contains(ev.Target.Role);
-            }
+            if (BetterRP.Singleton.Config.DamageIndicatorIsEnabled && ev.IsAllowed && ev.Player != null && !ev.Player.IsDead && ev.Amount > 0 && ev.Player != ev.Attacker)
+                ev.Attacker.ShowHint(BetterRP.Singleton.Config.DamageIndicatorHint.Replace("%damage", ev.Amount.ToString()).Replace("%player", ev.Player.Nickname), 6);
         }
     }
 }
